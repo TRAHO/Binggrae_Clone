d97bea0b1b00d65c74ec9f9614f26051c94a97e3
SDH <traho9750@naver.com>
2023-03-01 04:09:35 +0900

    전체 메뉴 버튼, 전체 메뉴 화면 구현

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,26 @@ function headerLangsBtn() {
             $('.header__lang-btn').removeClass('active');
             $('.header__langsWrap').stop().slideUp(500);
         }
-    })
+    });
+}
+
+// 전체 메뉴 버튼
+function headerFullMenuBtn() {
+    $('.header__fullMenu-btn').click(function(){
+        if($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $('.header__fullMenu').removeClass('active');
+            $('html, body').removeClass('fixed');
+            $('.header__navWrap').show();
+        }
+        else {
+            $(this).addClass('active');
+            $('html, body').addClass('fixed');
+            $('.header__navWrap').hide();
+            $('.header__fullMenu').addClass('active');
+        }
+    });
 }
 
 headerLangsBtn();
+headerFullMenuBtn();
